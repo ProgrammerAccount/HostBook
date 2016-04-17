ca446bdef293c2a90b6734935e846aa039df4e6d
Tymoteusz <tymek.janiak@wp.pl>
2016-04-17 12:02:05 +0200

    First

--- home.php
+++ home.php
@@ -8,20 +8,26 @@ if(!isset($_SESSION['zalogowany']))
 <html>
 <head>
   <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/css/fontello.css" type="text/css">
 <meta charset="utf-8"/>
 <title>HostBook</title>
 </head>
 <body>
   <main>
-    <h2 style="text-align:center;">Witaj w swoim konciku
+    <div id="header">
+     Witaj w swoim konciku
        <?php echo $_SESSION['name']; ?>
-     </h2>
+
+
+   </div>
    <div id="menu">
-     <a href="img.php"><div class="menu">Muzyka</div></a>
+     <a href="img.php"><div class="menu" style="border-left: 2px dotted blue;">Muzyka</div></a>
      <a href="img.php"><div class="menu">zdjecia</div></a>
      <a href="img.php"><div class="menu">Filmy</div></a>
      <a href="img.php"><div class="menu">Muzyka</div></a>
   </div>
+  Witaj w miejscu gdzie możesz zebrać ulubioną muzyka zdjecia i filmy.
+<i class="demo-icon icon-note"></i>
   </main>
 </body>
 </html>
